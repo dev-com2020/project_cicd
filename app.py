dd131ef13889e475c1bda486b1b2263225d19c0a
DEV-COM <kwp@tlen.pl>
2024-12-12 16:27:33 +0100

    Dodano nowe funkcje hello_world2 i hello_world3 oraz odpowiednie testy jednostkowe.

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 def hello_world():
     return "Hello World!"
 
+def hello_world2():
+    return "Python is awesome!"
+
+def hello_world3():
+    return "Selenium is awesome!"
+
 if __name__ == "__main__":
     print(hello_world())
+    print(hello_world2())
+    print(hello_world3())
--- test_app.py
+++ test_app.py
@@ -1,9 +1,16 @@
 import unittest
-from app import hello_world
+from app import hello_world, hello_world2, hello_world3
 
 class TestApp(unittest.TestCase):
+
     def test_hello(self):
         self.assertEqual(hello_world(),"Hello World!")
+    
+    def test_hello2(self):
+        self.assertEqual(hello_world2(),"Python is awesome!!!!")
+    
+    def test_hello3(self):
+        self.assertEqual(hello_world3(),"Selenium is awesome!")
 
 if __name__ == "__main__":
     unittest.main()
