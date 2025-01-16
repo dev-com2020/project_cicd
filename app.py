5c1d1f06f9c66b44a20c0defe2b0a8b86738d2e4
DEV-COM <kwp@tlen.pl>
2025-01-16 09:43:50 +0100

    Dodano funkcję calculate do sumowania dwóch liczb

--- app.py
+++ app.py
@@ -7,6 +7,9 @@ def hello_world2():
 def hello_world3():
     return "Selenium is awesome!"
 
+def calculate(a,b):
+    return a + b
+
 if __name__ == "__main__":
     print(hello_world())
     print(hello_world2())
